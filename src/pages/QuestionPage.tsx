3e13edf074a38165d5e8b9653dadbe6d0b5bd71a
ggombee <ggombee@gccompany.co.kr>
2022-12-18 02:35:25 +0900

    테스트 로직 완성하기

--- src/pages/QuestionPage.tsx
+++ src/pages/QuestionPage.tsx
@@ -1,15 +1,34 @@
 import React from "react";
 import styled from "styled-components";
 import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 import { QuestionData } from "../stores/Question/QuestionData";
 import Header from "../components/Header";
 
 function QuestionPage(): React.ReactElement {
   const [questionNo, setQuestionNo] = React.useState(0);
+  const [totalScore, setTotalScore] = React.useState([
+    { id: "EI", score: 0 },
+    { id: "SN", score: 0 },
+    { id: "TF", score: 0 },
+    { id: "JP", score: 0 },
+  ]);
+  const navigate = useNavigate();
 
-  const handleClickAnswer = () => {
-    setQuestionNo(questionNo + 1);
+  const handleClickAnswer = (ans: number, type: string) => {
+    const newScore = totalScore.map(s =>
+      s.id === type ? { id: s.id, score: s.score + ans } : s,
+    );
+
+    setTotalScore(newScore);
+    // 마지막 문제가 아닐경우
+    if (QuestionData.length !== questionNo + 1) {
+      setQuestionNo(questionNo + 1);
+      //마지막 문제일 경우
+    } else {
+      navigate("/result");
+    }
   };
 
   return (
@@ -20,12 +39,25 @@ function QuestionPage(): React.ReactElement {
         <ButtonGroup>
           <Button
             className="btn-warning"
-            style={{ marginRight: "20px" }}
-            onClick={handleClickAnswer}
+            style={{
+              marginRight: "20px",
+              width: "45%",
+              minHeight: "200px",
+              fontSize: "15pt",
+            }}
+            onClick={() => handleClickAnswer(1, QuestionData[questionNo].type)}
           >
             {QuestionData[questionNo].answera}
           </Button>
-          <Button className="btn-warning">
+          <Button
+            className="btn-warning"
+            style={{
+              width: "45%",
+              minHeight: "200px",
+              fontSize: "15pt",
+            }}
+            onClick={() => handleClickAnswer(0, QuestionData[questionNo].type)}
+          >
             {QuestionData[questionNo].answerb}
           </Button>
         </ButtonGroup>
@@ -62,7 +94,7 @@ const ButtonGroup = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: row;
-  margin-top: 20px;
+  margin-top: 40px;
   button {
     font-size: 18pt;
   }
