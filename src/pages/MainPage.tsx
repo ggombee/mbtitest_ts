c391d43eff7aec2b719574af8bc64d7935de940f
ggombee <ggombee@gccompany.co.kr>
2022-12-18 00:20:36 +0900

    홈화면 만들기

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,86 @@
 import React from "react";
+import styled from "styled-components";
+import { Button, Image } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+import PangImage from "../assets/ggompang.jpeg";
 
 function MainPage(): React.ReactElement {
-  return <div>이곳은 메인페이지입니다.</div>;
+  const navigate = useNavigate();
+
+  const handleClickButton = () => {
+    navigate("/question");
+  };
+
+  return (
+    <>
+      <Wrapper>
+        <Header>😼 예비집사 판별기 😼</Header>
+        <ContentsWrapper>
+          <Title>나에게 맞는 주인님은?!</Title>
+          <LogoImage>
+            <Image
+              className="rounded-circle"
+              src={PangImage}
+              width={350}
+              height={350}
+            />
+          </LogoImage>
+          <Desc>MBTI를 기반으로 하는 나랑 잘맞는 고양이 찾기!</Desc>
+          <Desc>내가 집사가 되서 고양이를 키운다면..?</Desc>
+          <Button
+            className="btn-danger"
+            onClick={handleClickButton}
+            style={{ fontSize: 25, marginTop: 20, marginBottom: 20 }}
+          >
+            테스트 시작하기
+          </Button>
+        </ContentsWrapper>
+      </Wrapper>
+    </>
+  );
 }
 
 export default MainPage;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  background: #fffacd;
+  font-family: "Jalnan";
+`;
+
+const Header = styled.div`
+  background: #ffa07a;
+  font-size: 40pt;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  font-family: "Jalnan";
+`;
+
+const ContentsWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 20px;
+`;
+
+const Title = styled.div`
+  margin-top: 20px;
+  font-size: 20pt;
+`;
+
+const LogoImage = styled.div`
+  width: 200;
+  height: 200;
+  margin-top: 10px;
+  margin-bottom: 20px;
+`;
+
+const Desc = styled.div`
+  font-size: 20pt;
+`;
